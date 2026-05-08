a723784d2242a8c8c9d1936036bbf00b8feba35c
Wilson Lin <wilslin21@gmail.com>
2026-05-08 11:29:16 +1000

    Make global zones always enabled for all modes

--- ScreenEstate/ScreenEstate/Models/Mode.swift
+++ ScreenEstate/ScreenEstate/Models/Mode.swift
@@ -9,7 +9,7 @@ struct Mode: Identifiable, Codable, Equatable {
     /// When nil, uses automatic left-to-right numbering. When set (even if empty), uses manual mode.
     var globalZoneAssignments: [String: Int]?
 
-    init(id: UUID, name: String, layouts: [MonitorLayout], globalZones: Bool = false, globalZoneAssignments: [String: Int]? = nil) {
+    init(id: UUID, name: String, layouts: [MonitorLayout], globalZones: Bool = true, globalZoneAssignments: [String: Int]? = nil) {
         self.id = id
         self.name = name
         self.layouts = layouts
@@ -17,13 +17,13 @@ struct Mode: Identifiable, Codable, Equatable {
         self.globalZoneAssignments = globalZoneAssignments
     }
 
-    // Backward compatibility: default globalZones to false if not present in saved data
+    // All modes now use global zones - ignore any stored false values
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decode(UUID.self, forKey: .id)
         name = try container.decode(String.self, forKey: .name)
         layouts = try container.decode([MonitorLayout].self, forKey: .layouts)
-        globalZones = try container.decodeIfPresent(Bool.self, forKey: .globalZones) ?? false
+        globalZones = true  // Always use global zones
         globalZoneAssignments = try container.decodeIfPresent([String: Int].self, forKey: .globalZoneAssignments)
     }
 }
